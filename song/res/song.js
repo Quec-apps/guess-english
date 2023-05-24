ec2a6d1b5b908f79caad6a2a7ea55fddbee5d59f
Quec-apps <79459289+Quec-apps@users.noreply.github.com>
2023-05-24 23:02:23 +0530

    Add files via upload

--- song/res/song.js
+++ song/res/song.js
@@ -1,83 +1,403 @@
 b = 0;
 
 b++;
-window["q"+b] = "Shape Of You"; //Ans1
-window["q1"+b] = " Runaway";
-window["q2"+b] = " Senorita";
-window["q3"+b] = " Lovely";
-window["q4"+b] = " Snap ";
-window["li"+b] = ' https://youtu.be/JGwWNGJdvx8';
+window["q"+b] = "Treat You Better"; //Ans1
+window["q1"+b] = "Hide Away";
+window["q2"+b] = "Blank Space";
+window["q3"+b] = "Hide Away";
+window["q4"+b] = "Never Forget You";
+window["li"+b] = ' https://youtu.be/lY2yjAdbvdQ';
 
 b++;
-window["q"+b] = " Lovely"; //Ans2
-window["q1"+b] = " Snap ";
-window["q2"+b] = " Please Don't Go";
-window["q3"+b] = " Senorita";
-window["q4"+b] = " Sugar & Brownies";
-window["li"+b] = ' https://youtu.be/V1Pl8CzNzCw';
+window["q"+b] = "There's Nothing Holdin' Me Back"; //Ans2
+window["q1"+b] = "One Last Time";
+window["q2"+b] = "Burn";
+window["q3"+b] = "Shake It Off";
+window["q4"+b] = "Say You Won't Let Go";
+window["li"+b] = ' https://youtu.be/dT2owtxkU8k';
 
 b++;
-window["q"+b] = " Middle Of The Night"; //Ans3
-window["q1"+b] = " Let Me Down Slowly";
-window["q2"+b] = " Let Me Down Slowly";
-window["q3"+b] = " Unstoppable";
-window["q4"+b] = " Let Me Down Slowly";
-window["li"+b] = ' https://youtu.be/oSHzUD-uqKY';
+window["q"+b] = "Sugar"; //Ans3
+window["q1"+b] = "The Scientist";
+window["q2"+b] = "Rockabye";
+window["q3"+b] = "Burn";
+window["q4"+b] = "Can't Feel My Face";
+window["li"+b] = ' https://youtu.be/09R8_2nJtjg';
 
 b++;
-window["q"+b] = " Unstoppable"; //Ans4
-window["q1"+b] = " Lovely";
-window["q2"+b] = "None";
-window["q3"+b] = " Please Don't Go";
-window["q4"+b] = " Please Don't Go";
-window["li"+b] = ' https://youtu.be/YaEG2aWJnZ8';
+window["q"+b] = "Marvin Gaya"; //Ans4
+window["q1"+b] = "The Scientist";
+window["q2"+b] = "The Heart Wants What It Wants";
+window["q3"+b] = "Pompeii";
+window["q4"+b] = "NO";
+window["li"+b] = ' https://youtu.be/igNVdlXhKcI';
 
 b++;
-window["q"+b] = " Runaway"; //Ans5
-window["q1"+b] = " Snap ";
-window["q2"+b] = " Please Don't Go";
-window["q3"+b] = " Unstoppable";
-window["q4"+b] = " Senorita";
-window["li"+b] = ' https://youtu.be/d_HlPboLRL8';
-
-b++;
-window["q"+b] = " Please Don't Go"; //Ans6
-window["q1"+b] = " Lovely";
-window["q2"+b] = " Runaway";
-window["q3"+b] = " Senorita";
-window["q4"+b] = " Lovely";
-window["li"+b] = ' https://youtu.be/S2oxFIsENgM';
-
-b++;
-window["q"+b] = " Snap "; //Ans7
-window["q1"+b] = " Let Me Down Slowly";
-window["q2"+b] = " Let Me Down Slowly";
-window["q3"+b] = " Senorita";
-window["q4"+b] = " Please Don't Go";
-window["li"+b] = ' https://youtu.be/Lo4_K4relMg';
-
-b++;
-window["q"+b] = " Let Me Down Slowly"; //Ans8
-window["q1"+b] = "None";
-window["q2"+b] = " Snap ";
-window["q3"+b] = " Senorita";
-window["q4"+b] = " Sugar & Brownies";
-window["li"+b] = ' https://youtu.be/s-bZD3O3P80';
-
-b++;
-window["q"+b] = " Sugar & Brownies"; //Ans9
-window["q1"+b] = " Let Me Down Slowly";
-window["q2"+b] = " Runaway";
-window["q3"+b] = " Runaway";
+window["q"+b] = "7 Years"; //Ans5
+window["q1"+b] = "Lean On";
+window["q2"+b] = "There's Nothing Holdin' Me Back";
+window["q3"+b] = "Hide Away";
 window["q4"+b] = "None";
-window["li"+b] = ' https://youtu.be/KuPg14K-e6s';
+window["li"+b] = ' https://youtu.be/LHCob76kigA';
 
 b++;
-window["q"+b] = " Senorita"; //Ans10
-window["q1"+b] = " Runaway";
-window["q2"+b] = " Sugar & Brownies";
-window["q3"+b] = " Middle Of The Night";
-window["q4"+b] = "Shape Of You";
-window["li"+b] = ' https://youtu.be/Pkh8UtuejGw';
+window["q"+b] = "Titanium"; //Ans6
+window["q1"+b] = "None";
+window["q2"+b] = "Roses";
+window["q3"+b] = "Rockabye";
+window["q4"+b] = "7 Years";
+window["li"+b] = ' https://youtu.be/9msKoxy7FcY';
+
+b++;
+window["q"+b] = "Despacito"; //Ans7
+window["q1"+b] = "The Scientist";
+window["q2"+b] = "Stay";
+window["q3"+b] = "Maps";
+window["q4"+b] = "Shake It Off";
+window["li"+b] = ' https://youtu.be/kJQP7kiw5Fk';
+
+b++;
+window["q"+b] = "Galway Girl"; //Ans8
+window["q1"+b] = "Radioactive";
+window["q2"+b] = "One Call Away";
+window["q3"+b] = "Demons";
+window["q4"+b] = "Rockabye";
+window["li"+b] = ' https://youtu.be/87gWaABqGYs';
+
+b++;
+window["q"+b] = "Never Forget You"; //Ans9
+window["q1"+b] = "Wildest Dreams";
+window["q2"+b] = "Hands To Myself";
+window["q3"+b] = "Bad Blood";
+window["q4"+b] = "The Scientist";
+window["li"+b] = ' https://youtu.be/GTyN-DB_v5M';
+
+b++;
+window["q"+b] = "Work From Home"; //Ans10
+window["q1"+b] = "Radioactive";
+window["q2"+b] = "Dare (La La La)";
+window["q3"+b] = "Blank Space";
+window["q4"+b] = "7 Years";
+window["li"+b] = ' https://youtu.be/5GL9JoH4Sws';
+
+b++;
+window["q"+b] = "Closer"; //Ans11
+window["q1"+b] = "7 Years";
+window["q2"+b] = "It Ain't Me";
+window["q3"+b] = "Treat You Better";
+window["q4"+b] = "Animals";
+window["li"+b] = ' https://youtu.be/0zGcUoRlhmw';
+
+b++;
+window["q"+b] = "Starving"; //Ans12
+window["q1"+b] = "Wildest Dreams";
+window["q2"+b] = "Bad Blood";
+window["q3"+b] = "Symphony";
+window["q4"+b] = "Demons";
+window["li"+b] = ' https://youtu.be/0zGcUoRlhmw';
+
+b++;
+window["q"+b] = "Symphony"; //Ans13
+window["q1"+b] = "Lean On";
+window["q2"+b] = "Wildest Dreams";
+window["q3"+b] = "Demons";
+window["q4"+b] = "None";
+window["li"+b] = ' https://youtu.be/aatr_2MstrI';
+
+b++;
+window["q"+b] = "Hips Don't Lie"; //Ans14
+window["q1"+b] = "Marvin Gaya";
+window["q2"+b] = "Kill Em With Kindness";
+window["q3"+b] = "Work From Home";
+window["q4"+b] = "Say You Won't Let Go";
+window["li"+b] = ' https://youtu.be/DUT5rEU6pqM';
+
+b++;
+window["q"+b] = "Love Myself"; //Ans15
+window["q1"+b] = "Sugar";
+window["q2"+b] = "Shake It Off";
+window["q3"+b] = "Rockabye";
+window["q4"+b] = "Galway Girl";
+window["li"+b] = ' https://youtu.be/bMpFmHSgC4Q';
+
+b++;
+window["q"+b] = "Rockabye"; //Ans16
+window["q1"+b] = "Sugar";
+window["q2"+b] = "Animals";
+window["q3"+b] = "Stole The Show feat";
+window["q4"+b] = "Stay";
+window["li"+b] = ' https://youtu.be/papuvlVeZg8';
+
+b++;
+window["q"+b] = "Stay"; //Ans17
+window["q1"+b] = "Cold Water";
+window["q2"+b] = "Despacito";
+window["q3"+b] = "Shake It Off";
+window["q4"+b] = "Work From Home";
+window["li"+b] = ' https://youtu.be/kTJczUoc26U';
+
+b++;
+window["q"+b] = "Shake It Off"; //Ans18
+window["q1"+b] = "Work From Home";
+window["q2"+b] = "None";
+window["q3"+b] = "Closer";
+window["q4"+b] = "Lean On";
+window["li"+b] = ' https://youtu.be/nfWlot6h_JM';
+
+b++;
+window["q"+b] = "Blank Space"; //Ans19
+window["q1"+b] = "Animals";
+window["q2"+b] = "Closer";
+window["q3"+b] = "Demons";
+window["q4"+b] = "Dare (La La La)";
+window["li"+b] = ' https://youtu.be/e-ORhEE9VVg';
+
+b++;
+window["q"+b] = "I Knew You Were Trouble"; //Ans20
+window["q1"+b] = "Stay";
+window["q2"+b] = "Shake It Off";
+window["q3"+b] = "Never Forget You";
+window["q4"+b] = "Shake It Off";
+window["li"+b] = ' https://youtu.be/vNoKguSdy4Y';
+
+b++;
+window["q"+b] = "It Ain't Me"; //Ans21
+window["q1"+b] = "Hide Away";
+window["q2"+b] = "Wildest Dreams";
+window["q3"+b] = "Love Myself";
+window["q4"+b] = "Hands To Myself";
+window["li"+b] = ' https://youtu.be/u3VTKvdAuIY';
+
+b++;
+window["q"+b] = "Kill Em With Kindness"; //Ans22
+window["q1"+b] = "Starving";
+window["q2"+b] = "Wildest Dreams";
+window["q3"+b] = "Starving";
+window["q4"+b] = "Hips Don't Lie";
+window["li"+b] = ' https://youtu.be/1_EHOeD79hc';
+
+b++;
+window["q"+b] = "The Heart Wants What It Wants"; //Ans23
+window["q1"+b] = "NO";
+window["q2"+b] = "Burn";
+window["q3"+b] = "Despacito";
+window["q4"+b] = "Marvin Gaya";
+window["li"+b] = ' https://youtu.be/ij_0p_6qTss';
+
+b++;
+window["q"+b] = "Hands To Myself"; //Ans24
+window["q1"+b] = "The Heart Wants What It Wants";
+window["q2"+b] = "Stay";
+window["q3"+b] = "None";
+window["q4"+b] = "NO";
+window["li"+b] = ' https://youtu.be/FMlcn-_jpWY';
+
+b++;
+window["q"+b] = "Radioactive"; //Ans25
+window["q1"+b] = "Titanium";
+window["q2"+b] = "Love Myself";
+window["q3"+b] = "Despacito";
+window["q4"+b] = "Roses";
+window["li"+b] = ' https://youtu.be/ktvTqknDobU';
+
+b++;
+window["q"+b] = "The Scientist"; //Ans26
+window["q1"+b] = "Cold Water";
+window["q2"+b] = "Starving";
+window["q3"+b] = "Animals";
+window["q4"+b] = "Animals";
+window["li"+b] = ' https://youtu.be/RB-RcX5DS5A';
+
+b++;
+window["q"+b] = "The Cure"; //Ans27
+window["q1"+b] = "Rockabye";
+window["q2"+b] = "Love Myself";
+window["q3"+b] = "Lean On";
+window["q4"+b] = "Kill Em With Kindness";
+window["li"+b] = ' https://youtu.be/I4IXxzioDAk';
+
+b++;
+window["q"+b] = "Say You Won't Let Go"; //Ans28
+window["q1"+b] = "The Heart Wants What It Wants";
+window["q2"+b] = "Cold Water";
+window["q3"+b] = "7 Years";
+window["q4"+b] = "Hips Don't Lie";
+window["li"+b] = ' https://youtu.be/0yW7w8F2TVA';
+
+b++;
+window["q"+b] = "Demons"; //Ans29
+window["q1"+b] = "Lean On";
+window["q2"+b] = "Blank Space";
+window["q3"+b] = "Bad Blood";
+window["q4"+b] = "Hands To Myself";
+window["li"+b] = ' https://youtu.be/mWRsgZuwf_8 ';
+
+b++;
+window["q"+b] = "Dare (La La La)"; //Ans30
+window["q1"+b] = "One Last Time";
+window["q2"+b] = "The Scientist";
+window["q3"+b] = "Treat You Better";
+window["q4"+b] = "Lean On";
+window["li"+b] = ' https://youtu.be/XkYAxGt-aUs';
+
+b++;
+window["q"+b] = "Stole The Show feat"; //Ans31
+window["q1"+b] = "Demons";
+window["q2"+b] = "Hide Away";
+window["q3"+b] = "Bad Blood";
+window["q4"+b] = "Blank Space";
+window["li"+b] = ' https://youtu.be/BgfcToAjfdc';
+
+b++;
+window["q"+b] = "Cold Water"; //Ans32
+window["q1"+b] = "Blank Space";
+window["q2"+b] = "Lean On";
+window["q3"+b] = "Burn";
+window["q4"+b] = "The Heart Wants What It Wants";
+window["li"+b] = ' https://youtu.be/nBtDsQ4fhXY';
+
+b++;
+window["q"+b] = "Something Just Like This"; //Ans33
+window["q1"+b] = "The Heart Wants What It Wants";
+window["q2"+b] = "Say You Won't Let Go";
+window["q3"+b] = "Animals";
+window["q4"+b] = "Animals";
+window["li"+b] = ' https://youtu.be/2LBBaD1aQuw';
+
+b++;
+window["q"+b] = "Cheap Thrills"; //Ans34
+window["q1"+b] = "Treat You Better";
+window["q2"+b] = "Love Myself";
+window["q3"+b] = "NO";
+window["q4"+b] = "Kill Em With Kindness";
+window["li"+b] = ' https://youtu.be/nYh-n7EOtMA';
+
+b++;
+window["q"+b] = "We Don't Talk Anymore"; //Ans35
+window["q1"+b] = "Maps";
+window["q2"+b] = "The Scientist";
+window["q3"+b] = "Hide Away";
+window["q4"+b] = "Cheap Thrills";
+window["li"+b] = ' https://youtu.be/i_yLpCLMaKk';
+
+b++;
+window["q"+b] = "Bad Blood"; //Ans36
+window["q1"+b] = "Say You Won't Let Go";
+window["q2"+b] = "NO";
+window["q3"+b] = "Shake It Off";
+window["q4"+b] = "Titanium";
+window["li"+b] = ' https://youtu.be/QcIy9NiNbmo';
+
+b++;
+window["q"+b] = "One Last Time"; //Ans37
+window["q1"+b] = "Wildest Dreams";
+window["q2"+b] = "Closer";
+window["q3"+b] = "Wildest Dreams";
+window["q4"+b] = "Lean On";
+window["li"+b] = ' https://youtu.be/BPgEgaPk62M';
+
+b++;
+window["q"+b] = "One Call Away"; //Ans38
+window["q1"+b] = "There's Nothing Holdin' Me Back";
+window["q2"+b] = "Titanium";
+window["q3"+b] = "We Don't Talk Anymore";
+window["q4"+b] = "It Ain't Me";
+window["li"+b] = ' https://youtu.be/BxuY9FET9Y4 ';
+
+b++;
+window["q"+b] = "Animals"; //Ans39
+window["q1"+b] = "Never Forget You";
+window["q2"+b] = "Love Myself";
+window["q3"+b] = "Burn";
+window["q4"+b] = "Love Myself";
+window["li"+b] = ' https://youtu.be/qpgTC9MDx1o';
+
+b++;
+window["q"+b] = "Chandelier"; //Ans40
+window["q1"+b] = "The Scientist";
+window["q2"+b] = "Radioactive";
+window["q3"+b] = "One Last Time";
+window["q4"+b] = "Sugar";
+window["li"+b] = ' https://youtu.be/2vjPBrBU-TM';
+
+b++;
+window["q"+b] = "Wildest Dreams"; //Ans41
+window["q1"+b] = "Animals";
+window["q2"+b] = "Marvin Gaya";
+window["q3"+b] = "Hips Don't Lie";
+window["q4"+b] = "Cold Water";
+window["li"+b] = ' https://youtu.be/IdneKLhsWOQ';
+
+b++;
+window["q"+b] = "NO"; //Ans42
+window["q1"+b] = "Maps";
+window["q2"+b] = "Maps";
+window["q3"+b] = "Never Forget You";
+window["q4"+b] = "We Don't Talk Anymore";
+window["li"+b] = ' https://youtu.be/cMTAUr3Nm6I';
+
+b++;
+window["q"+b] = "Lean On"; //Ans43
+window["q1"+b] = "The Cure";
+window["q2"+b] = "Hide Away";
+window["q3"+b] = "Roses";
+window["q4"+b] = "I Knew You Were Trouble";
+window["li"+b] = ' https://youtu.be/YqeW9_5kURI';
+
+b++;
+window["q"+b] = "Can't Feel My Face"; //Ans44
+window["q1"+b] = "Galway Girl";
+window["q2"+b] = "Hide Away";
+window["q3"+b] = "Sugar";
+window["q4"+b] = "I Knew You Were Trouble";
+window["li"+b] = ' https://youtu.be/KEI4qSrkPAs';
+
+b++;
+window["q"+b] = "Burn"; //Ans45
+window["q1"+b] = "Hands To Myself";
+window["q2"+b] = "Something Just Like This";
+window["q3"+b] = "Marvin Gaya";
+window["q4"+b] = "Hide Away";
+window["li"+b] = ' https://youtu.be/CGyEd0aKWZE';
+
+b++;
+window["q"+b] = "Hide Away"; //Ans46
+window["q1"+b] = "Can't Feel My Face";
+window["q2"+b] = "Hips Don't Lie";
+window["q3"+b] = "Shake It Off";
+window["q4"+b] = "Marvin Gaya";
+window["li"+b] = ' https://youtu.be/8zxhX381ZsA';
+
+b++;
+window["q"+b] = "Roses"; //Ans47
+window["q1"+b] = "Shake It Off";
+window["q2"+b] = "Something Just Like This";
+window["q3"+b] = "Demons";
+window["q4"+b] = "Work From Home";
+window["li"+b] = ' https://youtu.be/G5Mv2iV0wkU';
+
+b++;
+window["q"+b] = "Pompeii"; //Ans48
+window["q1"+b] = "NO";
+window["q2"+b] = "Cheap Thrills";
+window["q3"+b] = "Kill Em With Kindness";
+window["q4"+b] = "I Knew You Were Trouble";
+window["li"+b] = ' https://youtu.be/F90Cw4l-8NY';
+
+b++;
+window["q"+b] = "Maps"; //Ans49
+window["q1"+b] = "The Scientist";
+window["q2"+b] = "I Knew You Were Trouble";
+window["q3"+b] = "Burn";
+window["q4"+b] = "Demons";
+window["li"+b] = ' https://youtu.be/NmugSMBh_iI';
+
+b++;
+window["q"+b] = "Worth It"; //Ans50
+window["q1"+b] = "There's Nothing Holdin' Me Back";
+window["q2"+b] = "Animals";
+window["q3"+b] = "There's Nothing Holdin' Me Back";
+window["q4"+b] = "Kill Em With Kindness";
+window["li"+b] = ' https://youtu.be/YBHQbu5rbdQ';
 
 engSongLevelTotal = b;
