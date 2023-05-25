21c9bf6daa724619ab506fe28e0c194dbd603240
Quec-apps <79459289+Quec-apps@users.noreply.github.com>
2023-05-25 15:46:33 +0530

    Add files via upload

--- song/res/jquery.js
+++ song/res/jquery.js
@@ -63,6 +63,7 @@ $(document).ready(function () {
 		$("#noans2").text(window[`q2${levels}`]);
 		$("#noans3").text(window[`q3${levels}`]);
 		$("#noans4").text(window[`q4${levels}`]);
+		
 		$(".main-img").attr('src', `audio/${levels}.mp3`);
 		setTimeout(() => {
 			$("#ans-txt").html(window[`ans${levels}`]);
@@ -92,8 +93,68 @@ $(document).ready(function () {
 	}
 	Append();
 
+	window.onload = function () {
+		audio = document.getElementById("main-aud");
+	};
+	
 	$(".sound-img").click(function () {
-		document.getElementById("main-aud").play();
+		var files = this.files;
+		// audio.src = URL.createObjectURL(files[0]);
+		audio.load();
+		audio.play();
+		var context = new AudioContext();
+		var src = context.createMediaElementSource(audio);
+		var analyser = context.createAnalyser();
+
+		var canvas = document.getElementById("canvas");
+		// canvas.width = window.innerWidth;
+		// canvas.height = window.innerHeight;
+		var ctx = canvas.getContext("2d");
+
+		src.connect(analyser);
+		analyser.connect(context.destination);
+
+		analyser.fftSize = 256;
+
+		var bufferLength = analyser.frequencyBinCount;
+		console.log(bufferLength);
+
+		var dataArray = new Uint8Array(bufferLength);
+
+		var WIDTH = canvas.width;
+		var HEIGHT = canvas.height;
+
+		var barWidth = (WIDTH / bufferLength) * 2.5;
+		var barHeight;
+		var x = 0;
+
+		function renderFrame() {
+			requestAnimationFrame(renderFrame);
+
+			x = 0;
+
+			analyser.getByteFrequencyData(dataArray);
+
+			ctx.fillStyle = "#ffffffe0";
+			ctx.fillRect(0, 0, WIDTH, HEIGHT);
+
+			for (var i = 0; i < bufferLength; i++) {
+				barHeight = dataArray[i];
+
+				var r = barHeight + (25 * (i / bufferLength));
+				var g = 250 * (i / bufferLength);
+				var b = 50;
+
+				ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
+				ctx.fillRect(x, HEIGHT - barHeight, barWidth, barHeight);
+
+				x += barWidth + 1;
+			}
+		}
+
+		audio.play();
+		renderFrame();
+		// document.getElementById("main-aud").play();
 	});
 
 	$("#main-aud").on("ended", function () {
